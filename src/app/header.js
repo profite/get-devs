c6cd95dedfeecb01e9cb6ff90e8466a47d236005
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2016-02-11 17:35:07 -0200

    Criando HTML do header

--- src/app/header.js
+++ src/app/header.js
@@ -5,6 +5,31 @@ var ReactDOM = require('react-dom');
 
 module.exports = React.createClass({
     render: function(){
-        return <div>Hello header</div>
+        return <header>
+                    <nav>
+                        <div>
+                            <input type="text" name="search"></input>
+                        </div>
+                        <div>
+                            <img src="../img/profite-logo.png"/>
+                        </div>
+                        <div>
+                            <a href="#">Login / Cadastre-se</a>
+                        </div>
+                        <div>
+                            <i className="fa fa-shopping-cart"></i>
+                            <span>R$ </span>
+                        </div>
+                    </nav>
+                    <nav>
+                        <ul>
+                            <li>Novidades</li>
+                            <li>Masculino</li>
+                            <li>Feminino</li>
+                            <li>Marcas</li>
+                            <li>Ofertas</li>
+                        </ul>
+                    </nav>
+                </header>
     }
 })
