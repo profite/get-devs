26f3e9a12e407c239de6530263e90d41d1c6a941
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2016-02-12 21:57:44 -0200

    Ajustando CSS do carousel

--- src/app/carousel.js
+++ src/app/carousel.js
@@ -8,25 +8,27 @@ module.exports = React.createClass({
         return (
             <main>
                 <div className="carousel">
-                    <div className="carousel-icons">
+                    <div className="carousel-info">
                         <div className="left-arrow">
-                            <span className="fa fa-angle-left"></span>
+                            <span className="fa fa-angle-left fa-5x"></span>
                         </div>
-                        <div className="dots">
-                            <span className="fa fa-circle-o"></span>
-                            <span className="fa fa-circle"></span>
-                            <span className="fa fa-circle-o"></span>
+                        <div className="carousel-text">
+                            <p>Coleção</p>
+                            <p>Lorem Ipsum Dolor</p>
+                            <button>Conferir</button>
                         </div>
                         <div className="right-arrow">
-                            <span className="fa fa-angle-right"></span>
+                            <span className="fa fa-angle-right fa-5x"></span>
                         </div>
                     </div>
-                    <div className="carousel-text">
-                        <p>Coleção</p>
-                        <p>Lorem Ipsum Dolor</p>
-                        <button>Conferir</button>
+                    <div className="dots">
+                        <span className="fa fa-circle-o"></span>
+                        <span className="fa fa-circle"></span>
+                        <span className="fa fa-circle-o"></span>
                     </div>
                 </div>
+
+
             </main>
         )
     }
