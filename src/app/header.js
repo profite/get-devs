a5aad57e09393eb99f9682db533e1d66e763d3a0
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2016-02-12 18:56:24 -0200

    Estilizando header

--- src/app/header.js
+++ src/app/header.js
@@ -5,31 +5,38 @@ var ReactDOM = require('react-dom');
 
 module.exports = React.createClass({
     render: function(){
-        return <header>
-                    <nav>
-                        <div>
-                            <input type="text" name="search"></input>
-                        </div>
-                        <div>
-                            <img src="../img/profite-logo.png"/>
-                        </div>
-                        <div>
+        return (
+            <header>
+                <nav className="header-main">
+                    <div className="header-search">
+                        <span className="fa fa-search"></span>
+                        <input type="text" name="search"></input>
+                    </div>
+                    <div className="header-logo">
+                        <img src="../img/profite-logo.png"/>
+                    </div>
+                    <div className="header-user">
+                        <div className="header-login">
                             <a href="#">Login / Cadastre-se</a>
                         </div>
-                        <div>
-                            <i className="fa fa-shopping-cart"></i>
-                            <span>R$ </span>
+                        <div className="header-cart">
+                            <img src="../img/carrinho.png"/>
                         </div>
-                    </nav>
-                    <nav>
-                        <ul>
-                            <li>Novidades</li>
-                            <li>Masculino</li>
-                            <li>Feminino</li>
-                            <li>Marcas</li>
-                            <li>Ofertas</li>
-                        </ul>
-                    </nav>
-                </header>
+                        <div className="header-price">
+                            <strong>R$ 0,00</strong>
+                        </div>
+                    </div>
+                </nav>
+                <nav className="header-menu">
+                    <div className="header-links">
+                        <a>Novidades</a>
+                        <a>Masculino</a>
+                        <a>Feminino</a>
+                        <a>Marcas</a>
+                        <a>Ofertas</a>
+                    </div>
+                </nav>
+            </header>
+        )
     }
 })
