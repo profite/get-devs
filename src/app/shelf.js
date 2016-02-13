bf5b9c861bef3ceaf92433b4603c1d3ee91cfb68
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2016-02-12 22:24:32 -0200

    Ajustando prateleira

--- src/app/shelf.js
+++ src/app/shelf.js
@@ -5,6 +5,18 @@ var ReactDOM = require('react-dom');
 
 module.exports = React.createClass({
     render: function(){
-        return <div>Hello shelf</div>
+        return (
+            <section className="shelf">
+                <div className="shelf-img shelf-img-1">
+                    <img src="../img/prateleira-1.gif"/>
+                </div>
+                <div className="shelf-img shelf-img-2">
+                    <img src="../img/prateleira-2.gif"/>
+                </div>
+                <div className="shelf-img shelf-img-3">
+                    <img src="../img/prateleira-3.gif"/>
+                </div>
+            </section>
+        )
     }
 })
