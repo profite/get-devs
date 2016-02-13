35af0b5e1c620b29b9832a2ee6e31f1bc954d47a
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2016-02-13 16:02:23 -0200

    Criando formulário no HTML e ajeitando no CSS

--- src/app/registrationForm.js
+++ src/app/registrationForm.js
@@ -5,6 +5,26 @@ var ReactDOM = require('react-dom');
 
 module.exports = React.createClass({
     render: function(){
-        return <div>Hello registrationForm</div>
+        return (
+            <section>
+                <div className="registration-form">
+                    <div className="form-img">
+                        <img src="../img/desconto.gif" />
+                    </div>
+
+                    <div className="form-inputs">
+                        <div className="form-name">
+                            <a>Nome</a><br/>
+                            <input type="text" name="name"/>
+                        </div>
+                        <div className="form-email">
+                            <a>E-mail</a><br/>
+                        <input type="email" name="email"/>
+                        </div>
+                        <button className="form-btn">Cadastrar</button>
+                    </div>
+                </div>
+            </section>
+        )
     }
 })
