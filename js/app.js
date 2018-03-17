08b0bdd3ea60e6c8cf1c9bb1bea9d35e984a266d
Patrick Narciso <patrick.ncsilva@gmail.com>
2018-03-17 19:49:21 -0300

    inclusão de contants e config no app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 (function () {
-    'use strict'
+    'use strict';
 
-    angular.module('profite', ['ngRoute']);
+    angular.module('profite', ['ngRoute'])
+        .run(['$rootScope', function ($rootScope) {
+            $rootScope.listaCompra = [];
+        }])
+        .config(['$compileProvider', function ($compileProvider) {
+            $compileProvider.debugInfoEnabled(false);
+        }])
+        .constant('OrdenarOpt', [
+            {id: 1, label: "Mais Recentes"},
+            {id: 2, label: "Menor Preço"},
+            {id: 3, label: "Maior Preço"}
+        ]);
 })();
