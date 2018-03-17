5b72f85108b04bf557cd6d35adac611e3294826f
Patrick Narciso <patrick.ncsilva@gmail.com>
2018-03-17 19:40:18 -0300

    alterações na factory de models

--- js/models/models.js
+++ js/models/models.js
@@ -1,2 +1,10 @@
 angular.module('profite')
-       .factory()
+       .factory('Model', function (Cores, Tamanhos, Preco, Produtos, Carrinho) {
+           return {
+               Cores: Cores,
+               Tamanhos: Tamanhos,
+               Preco: Preco,
+               Produtos: Produtos,
+               Carrinho: Carrinho
+           }
+       });
