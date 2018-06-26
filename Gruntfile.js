073f71e0e10e7f4ed6878104491e44a0656b1557
LuAnderson <lucasand9@gmail.com>
2018-06-25 22:52:57 -0300

    Grunt - end config minify html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,18 @@
 module.exports = function (grunt) {
-  grunt.config.init({
-    htmlmin: {
-      file: {
-        options: {
-          removeComments: true,
-          collapseWhitespace: true
-        },
+  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+
+  grunt.initConfig({
+    minifyHtml: {
+      options: {
+        cdata: true
+      },
+      dist: {
         files: {
-          src: 'src/index.html',
-          dest: 'dist/'
+          'dist/index.html': 'index.html'
         }
       }
-    }
+    },
   });
-};
+
+  grunt.registerTask('default', ['minifyHtml']);
+}; 
