299f775456aaea69f6f0b7cb439b72ccff1bccf9
LuAnderson <lucasand9@gmail.com>
2018-06-26 20:51:42 -0300

    Grunt - html/less/js/watch add

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,48 @@
 module.exports = function (grunt) {
-  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+  require('load-grunt-tasks')(grunt); 
 
   grunt.initConfig({
     minifyHtml: {
       options: {
-        cdata: true
+        cdata: true,
+        removeComments: true,
+        collapseWhitespace: true
       },
       dist: {
         files: {
-          'dist/index.html': 'index.html'
+          'dist/index.min.html': 'index.html'
         }
       }
     },
+    uglify: {
+      my_target: {
+        files: {
+          'dist/js/script.min.js': ['src/js/script.js', 'src/js/script.1.js']
+        }
+      }
+    },
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          'dist/css/header.css': 'src/style/less/header.less'
+        }
+      },
+    },
+    watch: {
+      styles: {
+        files: ['src/style/less/*.less'], 
+        tasks: ['less'],
+        options: {
+          debounceDelay: 250,
+        }
+      }
+    }
   });
 
-  grunt.registerTask('default', ['minifyHtml']);
+  grunt.registerTask('default', ['minifyHtml', 'uglify', 'less', 'watch']);
 }; 
---
+++ src/js/script.1.js
@@ -0,0 +1,1 @@
+console.log("My name is Lucas");
