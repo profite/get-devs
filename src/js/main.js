68f966b2ebd33e6dbe1046bf4fe6271f370491c7
LuAnderson <lucasand9@gmail.com>
2018-07-02 04:17:34 -0300

    Funcionalidade de adicionar ao carrinho

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,19 @@ var click = document.getElementsByClassName("checkbox");
 
 console.log("click", click);
 
-function someFunction(obj,abc) {
-    console.log(abc);
-    console.log(obj.getAttribute('class'));   
+// function someFunction(obj, abc) {
+//     console.log(abc);
+//     console.log(obj.getAttribute('class'));
+// }
+
+function codeReset() {
+    console.log("oi")
+    document.getElementById("compra").style.display = "none";
 }
+
+var clicks = 0;
+function onClick() {
+    clicks += 1;
+    document.getElementById("compra").style.display = "block";
+    document.getElementById("compra").innerHTML = clicks;
+};
