0be304c4ee5bc0b8d482edc2fac4b49b9044d760
LuAnderson <lucasand9@gmail.com>
2018-07-13 00:00:26 -0300

    Preparando grunt para refatoração do código e finalização

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,13 +29,26 @@ module.exports = function (grunt) {
           optimization: 2 
         },
         files: { 
-          'dist/css/style.min.css': 'src/style/imports.less',
+          'dist/css/style.min.css': 
+          ['src/style/reset.less',
+           'src/style/layout/footer.less',
+            'src/style/layout/header.less',
+             'src/style/layout/menu-product.less',
+              'src/style/layout/product.less',
+               'src/style/layout/search-options.less',
+                'src/style/helpers/filter.less',
+                 'src/style/base/functionality.less',
+                  'src/style/base/grid.less'],
         }
       },
     },
     watch: {
       styles: {
-        files: ['src/style/less/*.less'],
+        files: [
+          'src/style/base/*.less',
+           'src/style/helpers/*.less',
+            'src/style/layout/*.less',
+             'src/style/reset.less'],
         tasks: ['less'],
         options: {
           debounceDelay: 250,
