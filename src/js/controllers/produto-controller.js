5dc4942a873969ced4a0ac17ad5a98eb7ffb60ea
LuAnderson <lucasand9@gmail.com>
2018-07-24 21:42:32 -0300

    Removendo HTML e JS não padronizado

--- src/js/controllers/produto-controller.js
+++ src/js/controllers/produto-controller.js
@@ -1,27 +1,20 @@
+profiteDesafio.controller('VestidoController', function ($scope, $http) {
+    $scope.produtos = [];
+    $scope.myNumber = 6; // valor inicial 6
+    $scope.getNumber = function (num) {
+        return $scope.myNumber;
+    }
 
-var profiteDesafio = angular.module("profiteDesafio", []);
+    $scope.loadMoreProduct = function ($event) {
+        var element = angular.element($event.target);
+        $scope.myNumber = $scope.myNumber + 2;
+    };
 
-(function (app) {
-    "use strict";
-    app.controller('VestidoController', function ($scope, $http) {
-        $scope.produtos = [];
-        $scope.myNumber = 6; // valor inicial 6
-        $scope.getNumber = function (num) {
-            return $scope.myNumber;
-        }   
-
-        $scope.loadMoreProduct = function ($event) {
-            var element = angular.element($event.target);
-            $scope.myNumber = $scope.myNumber + 2;
-        };
-
-        $http.get('https://api.myjson.com/bins/6ugdw')
-            .success(function (produtos) {
-                $scope.produtos = produtos;
-            })
-            .error(function (erro) {
-                console.log("Foi encontro um erro no donwload dos produtos: ", erro);
-            });
-    });
-
-})(profiteDesafio);
+    $http.get('https://api.myjson.com/bins/6ugdw')
+        .success(function (produtos) {
+            $scope.produtos = produtos;
+        })
+        .error(function (erro) {
+            console.log("Foi encontro um erro no donwload dos produtos: ", erro);
+        });
+});
---
+++ src/js/main.js
@@ -0,0 +1,1 @@
+var profiteDesafio = angular.module("profiteDesafio", []);
